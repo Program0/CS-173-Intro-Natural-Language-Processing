821d5b4d389a1e7d9ff5fc1844b5c310dc310b57
Marlo Zeroth <Program0@users.noreply.github.com>
2019-06-07 17:24:35 -0700

    Revised to use most current coreNLP wrapper
    Revised to use the most current Stanford coreNLP python wrapper.

--- assignment5/concordance.py
+++ assignment5/concordance.py
@@ -14,7 +14,8 @@ import numpy as np
 import pandas as pd
 import csv
 import re
-from stanfordnlp.server import CoreNLPClient
+#from stanfordnlp.server import CoreNLPClient
+from stanfordcorenlp import StanfordCoreNLP
 
 # Create column names for the dataframe
 data_names = ["id", "title", "publication",
@@ -32,86 +33,62 @@ for name in filelist:
     files += " " + name
 
 
-# Get only the parts of the corpus
-name_part = re.findall(r'articles[0-9][.]csv', files)
+# Get only the parts of the corpus. For now just only process files with 1
+name_part = re.findall(r'articles[1][.]csv', files)
 
 
 numFiles = 0
 numSentences = 0
+numArticles = 0
 
-#article = defaultdict()
-print("Hello world")
-print(filePath)
-print(files)
+entity_index = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
+columns = ["entity", "type", "publisher", "author", "article", "line"]
+rows = list()
 
 # set up the client
-with CoreNLPClient(annotators=['tokenize','ssplit','pos','lemma','ner', 'parse', 'depparse','coref'], timeout=30000, memory='4G') as client:
+nlp = StanfordCoreNLP(r'C:\stanford-corenlp-full-2018-10-05')
 
-    for file in name_part:
-        # Read the log file
+ner_types = {"LOCATION", "STATE_OR_PROVINCE", "CITY", "COUNTRY", 
+             "ORGANIZATION", "PERSON", "IDEOLOGY", "TITLE"}
+
+for file in name_part:
+    # Read the log file
+    try:
         df = pd.read_csv(filePath + "/" + file)
-        print(filePath + "/" + file)
-        print("opening files")
-        # Get the number of lines in the dataframe
-        print("Number of records in dataframe: %d" % (df.shape[0]))
-        print(df.iloc[0])
-        content = df.at[0, "content"]
-        
+    except SyntaxError:
+        print("Syntax error on file: ", file)
+    print(filePath + "/" + file)
+    print("opening files")
+    # Get the number of lines in the dataframe
+    print("Number of articles in file: %d" % (df.shape[0]))
+    records = df.shape[0]
+    numFiles+=1
+    for record in range(0, records):
+        numArticles+=1        
+        if(numArticles > 50):
+            break
+        publisher = df.at[record, 
+                          "publication"]
+        article = df.at[record, "title"]
+        content = df.at[record, "content"]
+        author = df.at[record, "author"]
         lines = content.strip().split(".")
-        text = lines[0]
-        print(text)
-        ann = client.annotate(text)
-
-        # get the first sentence
-        sentence = ann.sentence[0]
-        
-        # get the first token of the first sentence
-        print('---')
-        print('first token of first sentence')
-        token = sentence.token[0]
-        print(token)
-
-        # get the part-of-speech tag
-        print('---')
-        print('part of speech tag of token')
-        token.pos
-        print(token.pos)
-
-        # get the named entity tag
-        print('---')
-        print('named entity tag of token')
-        print(token.ner)
-
-        # get an entity mention from the first sentence
-        print('---')
-        print('first entity mention in sentence')
-        print(sentence.mentions[0])
-
-        # access the coref chain
-        print('---')
-        print('coref chains for the example')
-        print(ann.corefChain)
-
-        # Use tokensregex patterns to find who wrote a sentence.
-        pattern = '([ner: PERSON]+) /wrote/ /an?/ []{0,3} /sentence|article/'
-        matches = client.tokensregex(text, pattern)
-        # sentences contains a list with matches for each sentence.
-        assert len(matches["sentences"]) == 3
-        # length tells you whether or not there are any matches in this
-        assert matches["sentences"][1]["length"] == 1
-        # You can access matches like most regex groups.
-        matches["sentences"][1]["0"]["text"] == "Chris wrote a simple sentence"
-        matches["sentences"][1]["0"]["1"]["text"] == "Chris"
-
-        # Use semgrex patterns to directly find who wrote what.
-        pattern = '{word:wrote} >nsubj {}=subject >dobj {}=object'
-        matches = client.semgrex(text, pattern)
-        # sentences contains a list with matches for each sentence.
-        assert len(matches["sentences"]) == 3
-        # length tells you whether or not there are any matches in this
-        assert matches["sentences"][1]["length"] == 1
-        # You can access matches like most regex groups.
-        matches["sentences"][1]["0"]["text"] == "wrote"
-        matches["sentences"][1]["0"]["$subject"]["text"] == "Chris"
-        matches["sentences"][1]["0"]["$object"]["text"] == "sentence"
-
+        print("Processing article: %d %s" % (numArticles, article))
+        line_number = 0
+        for line in lines:
+            line_number+=1
+            entities = nlp.ner(line)
+            for entity in entities:
+                if entity[1] in ner_types:
+                    rows.append([entity[0], entity[1], 
+                                publisher, author, article, 
+                                line_number])
+                    entity_index[(entity[0], entity[1], publisher)].append([author, article, line_number])
+                
+concordance = pd.DataFrame(rows, columns=columns)
+print("Total entities: %d" % concordance.shape[0])
+nlp.close()
+print(concordance) 
+file_name = filePath + "/concordance.csv"
+concordance.to_csv(path_or_buf=file_name, header=None, index=None)
+ 
